08df86f22b2e90db468275a4c75a33087c2b2279
Alejandro E. Rendon <alejorendon@utp.edu.co>
2018-07-05 16:30:38 -0500

    Basic block structure

---
+++ src/block.py
@@ -0,0 +1,14 @@
+from hashlib import sha256
+import json
+import time
+
+class Block:
+    def __init__(self, index, transactions, timestamp):
+        self.index = []
+        self.transactions = transactions
+        self.timestamp = timestamp
+        self.previous_hash = previous_hash
+
+    def compute_hash(self):
+        block_string = json.dumps(self.__dict__, sort_keys=True)
+        return sha256(block_string.enconde()).hexdigest()
