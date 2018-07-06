2f738f7599ea2dba3401bb51ad458640b4d4311d
Alejandro E. Rendon <alejorendon@utp.edu.co>
2018-07-06 15:21:06 -0500

    Basic structure of Blockchain class

---
+++ src/blockchain.py
@@ -0,0 +1,12 @@
+from block import Block
+
+class Blockchain:
+    def __init__(self):
+        self.unconfirmed_transactions = []
+        self.chain = []
+        self.create_genesis_block()
+
+    def create_genesis_block(self):
+        genesis_block = Block(0, [], time.time(), "0")
+        genesis_block.hash = genesis_block.compute_hash()
+        self.chain.append(genesis_block)
