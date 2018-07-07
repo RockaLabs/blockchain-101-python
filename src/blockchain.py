0e7ea329d29e0df86554836a75c6f9315330607c
Alejandro E. Rendon <alejorendon@utp.edu.co>
2018-07-06 21:24:06 -0500

    Add block to chain

--- src/blockchain.py
+++ src/blockchain.py
@@ -1,12 +1,46 @@
 from block import Block
+import time
 
 class Blockchain:
+    difficulty = 2
+
     def __init__(self):
         self.unconfirmed_transactions = []
         self.chain = []
         self.create_genesis_block()
 
+    @property
+    def last_block(self):
+        return self.chain(-1)
+
     def create_genesis_block(self):
         genesis_block = Block(0, [], time.time(), "0")
         genesis_block.hash = genesis_block.compute_hash()
         self.chain.append(genesis_block)
+
+    def proof_of_work(self, block):
+        block.nonce = 0
+
+        computed_hash = block.compute_hash()
+
+        while not computed_hash.startswith('0' * Blockchain.difficulty):
+            block.nonce += 1
+            computed_hash = block.compute_hash()
+ 
+        return computed_hash
+
+    def add_block(self, block, proof):
+        previous_hash = self.last_block.hash
+
+        if previous_hash != block.previous_hash:
+            return False
+
+        if not self.is_valid_proof(block, proof):
+            return False
+
+        block.hash = proof
+        self.chain.append(block)
+        return True
+
+    def is_valid_proof(self, block, block_hash):
+        return (block_hash.startwith('0' * Blockchain.difficulty) and block_hash == block.compute_hash())
